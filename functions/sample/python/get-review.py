97ce5995e934854307de8cb64a398d33ef41ae50
Rahul R <40175416+rahul7795@users.noreply.github.com>
2023-06-10 20:53:08 +0530

    Create get-review.py

---
+++ functions/sample/python/get-review.py
@@ -0,0 +1,29 @@
+"""
+Get the total list of reviews
+HTTP Method : GET
+"""
+
+import sys 
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict): 
+    authenticator = IAMAuthenticator("API KEY TO BE ENTERED")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("URL TO BE ENTERED")
+    response = service.post_find(
+                db='reviews',
+                selector={'dealership': {'$eq': int(dict['id'])}},
+            ).get_result()
+    try: 
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True) 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
