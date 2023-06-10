8d675a59f89e66fe5d0ee1cda3acf292dc96180c
Rahul R <40175416+rahul7795@users.noreply.github.com>
2023-06-10 20:53:40 +0530

    Create post-review.py

---
+++ functions/sample/python/post-review.py
@@ -0,0 +1,26 @@
+"""
+Post a new review in the database
+HTTP Method : POST
+"""
+
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+def main(dict):
+    authenticator = IAMAuthenticator("API KEY TO BE ENTERED")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("URL TO BE ENTERED")
+    response = service.post_document(db='reviews', document=dict["review"]).get_result()
+    try:
+    # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
+        
