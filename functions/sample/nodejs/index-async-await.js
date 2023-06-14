c0239cfda9314375f24f377ac74734f6647535fb
Rahul R <40175416+rahul7795@users.noreply.github.com>
2023-06-14 10:38:15 +0530

    Update index-async-await.js

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -6,11 +6,11 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+      const authenticator = new IamAuthenticator({ apikey: "mZBzXoWJcsH_RBkm61gW36PKvvHaqJLVbYXZ2lVpjBTU" })
       const cloudant = CloudantV1.newInstance({
           authenticator: authenticator
       });
-      cloudant.setServiceUrl(params.COUCH_URL);
+      cloudant.setServiceUrl("https://b4fde291-319d-4463-9c6b-771132a03dec-bluemix.cloudantnosqldb.appdomain.cloud");
       try {
         let dbList = await cloudant.getAllDbs();
         return { "dbs": dbList.result };
