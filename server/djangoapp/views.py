66c55481275137ae5b3854df30f79e70c5b44c26
Rahul R <40175416+rahul7795@users.noreply.github.com>
2023-06-18 13:34:45 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -5,8 +5,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 from .models import CarModel
 # from .restapis import related methods
-from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, get_dealer_from_cf_by_id, post_request
-
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -16,27 +15,20 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
 def about(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-
 # Create a `contact` view to return a static contact page
-#def contact(request):
 def contact(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
 def login_request(request):
     context = {}
     if request.method == "POST":
@@ -55,7 +47,6 @@ def login_request(request):
         return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
 def logout_request(request):
     context = {}
     # get user from session id
@@ -65,7 +56,6 @@ def logout_request(request):
     return render(request, 'djangoapp/index.html', context)
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
 def registration_request(request):
     context = {}
     # rend if it is a GET req
@@ -94,76 +84,57 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        context = {}
-        url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-dealership"
-        dealerships = get_dealers_from_cf(url)
-        context["dealership_list"] = dealerships
+        url = "https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/a9220b6d6b26f1eb3b657a98770b743616f7d4cd223b89cd1ca4e88ab49bdb92/api/dealership"
+        # Get dealers from the URL
+        context = {
+            "dealerships": get_dealers_from_cf(url),
+        }
         return render(request, 'djangoapp/index.html', context)
+        
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
 def get_dealer_details(request, dealer_id):
     if request.method == "GET":
-        context = {}
-        url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-review"
-        reviews = get_dealer_reviews_from_cf(url, dealer_id)
-        context["reviews"] = reviews
-        dealer = get_dealer_from_cf_by_id(
-            "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-dealership", dealer_id)
-        context["dealer"] = dealer
-
+        url_r = f"https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/a9220b6d6b26f1eb3b657a98770b743616f7d4cd223b89cd1ca4e88ab49bdb92/api/review?dealerId={dealer_id}"
+        url_ds = f"https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/a9220b6d6b26f1eb3b657a98770b743616f7d4cd223b89cd1ca4e88ab49bdb92/api/dealership?dealerId={dealer_id}"
+        # Get dealers from the URL
+        context = {
+            "dealer": get_dealers_from_cf(url_ds)[0],
+            "reviews": get_dealer_reviews_from_cf(url_r, dealer_id),
+        }
         return render(request, 'djangoapp/dealer_details.html', context)
-
-# def get_dealer_details(request, id):
-#     if request.method == "GET":
-#         context = {}
-#         dealer_url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-dealership"
-#         dealer = get_dealers_from_cf(dealer_url, id=id)
-#         context["dealer"] = dealer
-    
-#         review_url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-review"
-#         reviews = get_dealer_reviews_from_cf(review_url, id=id)
-#         print(reviews)
-#         context["reviews"] = reviews
-        
-#         return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
 def add_review(request, dealer_id):
-    context = {}
     if request.method == "GET":
-        url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/get-dealership"
-        dealer = get_dealer_from_cf_by_id(url, dealer_id)
-        cars = CarModel.objects.filter(dealer_id=dealer_id)
-        context["cars"] = cars
-        context["dealer"] = dealer
+        url = f"https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/a9220b6d6b26f1eb3b657a98770b743616f7d4cd223b89cd1ca4e88ab49bdb92/api/dealership?dealerId={dealer_id}"
+        # Get dealers from the URL
+        context = {
+            "cars": CarModel.objects.all(),
+            "dealer": get_dealers_from_cf(url)[0],
+        }
+        print(context)
         return render(request, 'djangoapp/add_review.html', context)
-
     if request.method == "POST":
-        url = "https://us-south.functions.appdomain.cloud/api/v1/web/4c0025f7-6c55-4ff7-b978-1992cc6542bc/dealership-package/post-review"      
-        if 'purchasecheck' in request.POST:
-            was_purchased = True
-        else:
-            was_purchased = False
-        cars = CarModel.objects.filter(dealer_id=dealer_id)
-        for car in cars:
-            if car.id == int(request.POST['car']):
-                review_car = car  
-        review = {}
-        review["time"] = datetime.utcnow().isoformat()
-        review["name"] = request.POST['name']
-        review["dealership"] = dealer_id
-        review["review"] = request.POST['content']
-        review["purchase"] = was_purchased
-        review["purchase_date"] = request.POST['purchasedate']
-        review["car_make"] = review_car.make.name
-        review["car_model"] = review_car.name
-        review["car_year"] = review_car.year.strftime("%Y")
-        json_payload = {}
-        json_payload["review"] = review
-        response = post_request(url, json_payload)
-        return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+        form = request.POST
+        review = {
+            "name": f"{request.user.first_name} {request.user.last_name}",
+            "dealership": dealer_id,
+            "review": form["content"],
+            "purchase": form.get("purchasecheck"),
+            }
+        if form.get("purchasecheck"):
+            review["purchasedate"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            car = CarModel.objects.get(pk=form["car"])
+            review["car_make"] = car.car_make.name
+            review["car_model"] = car.name
+            review["car_year"]= car.year.strftime("%Y")
+        json_payload = {"review": review}
+        URL = 'https://service.eu.apiconnect.ibmcloud.com/gws/apigateway/api/a9220b6d6b26f1eb3b657a98770b743616f7d4cd223b89cd1ca4e88ab49bdb92/api/review'
+        post_request(URL, json_payload, dealerId=dealer_id)
+    return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+    
